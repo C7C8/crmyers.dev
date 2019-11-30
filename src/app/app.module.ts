1e8f27d81b5b940ba2f88890d4555a870377fb0b
Sourec <tothemyers@gmail.com>
2019-11-29 20:31:45 -0500

    Add cookie saving site type preference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
 import { ProfessionalPersonalPopupComponent } from './professional-personal-popup/professional-personal-popup.component';
 import { LoaderComponent } from './loader.component';
 import { RootComponent } from './root.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
     declarations: [
@@ -34,7 +35,7 @@ import { RootComponent } from './root.component';
         MatCardModule
     ],
     entryComponents: [ ProfessionalPersonalPopupComponent ],
-    providers: [],
+    providers: [ CookieService ],
     bootstrap: [ RootComponent ]
 })
 export class AppModule { }
--- src/app/loader.component.ts
+++ src/app/loader.component.ts
@@ -2,6 +2,7 @@ import { Component, AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { ProfessionalPersonalPopupComponent } from './professional-personal-popup/professional-personal-popup.component';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-loader',
@@ -10,9 +11,15 @@ import { Router } from '@angular/router';
 })
 export class LoaderComponent implements AfterViewInit {
 
-  constructor(public dialog: MatDialog, public router: Router) { }
+  constructor(private dialog: MatDialog, private router: Router, private cookieService: CookieService) { }
 
     async ngAfterViewInit() {
+      const cookie = this.cookieService.get('user-type');
+      if (cookie) {
+          this.router.navigate(cookie === 'human' ? ['/personal'] : ['/professional']);
+          return;
+      }
+
       const dialogRef = this.dialog.open(ProfessionalPersonalPopupComponent, {
           width: '66%',
           height: '66%',
@@ -20,11 +27,12 @@ export class LoaderComponent implements AfterViewInit {
       });
 
       const result: string = await dialogRef.afterClosed().toPromise();
-
       if (result === 'personal') {
-          this.router.navigate(['/personal']);
+          this.cookieService.set('user-type', 'human', 2147483647);
+          await this.router.navigate(['/personal']);
       } else {
-          this.router.navigate(['/professional']);
+          this.cookieService.set('user-type', 'professional', 2147483647);
+          await this.router.navigate(['/professional']);
       }
     }
 
