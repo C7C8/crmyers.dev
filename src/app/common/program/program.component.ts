ea2df47c009f0fb18d4820d0ff29dfdcbeca5d60
Sourec <tothemyers@gmail.com>
2020-01-11 19:44:35 -0500

    Move program year to just after the title

--- src/app/common/program/program.component.ts
+++ src/app/common/program/program.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-program',
@@ -13,9 +13,16 @@ export class ProgramComponent implements OnInit {
     @Input() link: string;
     @Input() video: string;
     @Input() image: string;
+    @ViewChild('vid', {static: false}) vid;
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    pauseVideo(): void {
+        if (this.vid) {
+            this.vid.nativeElement.pause();
+        }
     }
 }
