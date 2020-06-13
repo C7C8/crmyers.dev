66a8ea41a4b98d17a3b62be496e92e884864b6bc
Sourec <tothemyers@gmail.com>
2020-06-12 21:29:00 -0400

    Remove img prefix from assets/personal/*

--- src/app/personal/hobbies/latex/LatexCarouselImages.ts
+++ src/app/personal/hobbies/latex/LatexCarouselImages.ts
@@ -2,46 +2,46 @@ import { Image } from '../../../common/defs';
 
 export const LatexCarouselImages: Image[] = [
     {
-        image: '/assets/personal/img/latex/resume.png',
+        image: '/assets/personal/latex/resume.png',
         alt: 'Personal resume',
         caption: 'My personal resume, made with the ModernCV package'
     },
     {
-        image: '/assets/personal/img/latex/mqp-one-pager.png',
+        image: '/assets/personal/latex/mqp-one-pager.png',
         alt: 'MQP one-pager',
         caption: 'The one-pager project proposal I threw together to present to my team\'s advisor. We... never actually used it, ' +
             'but it still looks pretty!'
     },
     {
-        image: '/assets/personal/img/latex/poems.png',
+        image: '/assets/personal/latex/poems.png',
         alt: 'Poem typesetting example',
         caption: 'Poem example from my poetry final project, titled "Poems \'n\' Stuff, I Guess". All line numbers were automatic!'
     },
     {
-        image: '/assets/personal/img/latex/bme3300.png',
+        image: '/assets/personal/latex/bme3300.png',
         alt: 'BME 3300 report typesetting',
         caption: 'A typesetting project I did on a friend\'s BME 3300 final report. Text blurred as it\'s not mine.'
     },
     {
-        image: '/assets/personal/img/latex/bar-association.png',
+        image: '/assets/personal/latex/bar-association.png',
         alt: 'Bar association campaign chapter',
         caption: 'Typeset DND "Bar Association" module that my DND group plays through. I update it as we go along.'
     },
     {
-        image: '/assets/personal/img/latex/iqp.png',
+        image: '/assets/personal/latex/iqp.png',
         alt: 'IQP report',
         caption: 'The final report from my IQP (project-abroad of sorts) team in winter 2019. The final product was ~40 pages long and ' +
             'used biblatex for automatic citations, plus hyperref for decent cross references.'
     },
     {
-        image: '/assets/personal/img/latex/mqp.png',
+        image: '/assets/personal/latex/mqp.png',
         alt: 'MQP report',
         caption: 'The final report my team and I wrote for our major qualifying project, a computer science research project. One of the ' +
             'really useful features of LaTeX shone here - we were able to automagically generate a list of acronyms and set ' +
             'linked cross references to the definition, plus automatic acronym expansion on the first use of the acronym.'
     },
     {
-        image: '/assets/personal/img/latex/paris-knight.png',
+        image: '/assets/personal/latex/paris-knight.png',
         alt: 'Book typesetting',
         caption: 'Typesetting project I did on my dad\'s book, "The Dark Star of Paris Knight". I formatted it for an actual standard' +
             ' book size, complete with alternating pages and page numbers, and even fancy drop caps and text breaks.'
--- src/app/personal/home/HomeCarouselImages.ts
+++ src/app/personal/home/HomeCarouselImages.ts
@@ -2,47 +2,47 @@ import { Image } from '../../common/defs';
 
 export const HomeCarouselImages: Image[] = [
     {
-        image: '/assets/personal/img/code-projects/apisite.png',
+        image: '/assets/personal/code-projects/apisite.png',
         alt: 'API hosting site',
         caption: 'API hosting website built to help CS 3733 at WPI.'
     },
     {
-        image: '/assets/personal/img/blender/cant-outrun-bots.jpg',
+        image: '/assets/personal/blender/cant-outrun-bots.jpg',
         alt: 'Robots destroying a city',
         caption: 'Art of some Amazon robots knocking down a city.'
     },
     {
-        image: '/assets/personal/img/home-carousel/computer-usage-viz.png',
+        image: '/assets/personal/home-carousel/computer-usage-viz.png',
         alt: 'heatmap',
         caption: 'Heatmap visualization of when I used my laptop in winter 2017.'
     },
     {
-        image: '/assets/personal/img/blender/enterprise-ad-notexture.jpg',
+        image: '/assets/personal/blender/enterprise-ad-notexture.jpg',
         alt: 'USS Enterprise',
         caption: 'Untextured, unfinished mashup of the Enterprise and the Enterprise D.'
     },
     {
-        image: '/assets/personal/img/blender/magnet-toy.jpg',
+        image: '/assets/personal/blender/magnet-toy.jpg',
         alt: 'magnet toy',
         caption: 'Render of a magnetic toy sitting on my desk.'
     },
     {
-        image: '/assets/personal/img/code-projects/nball-sim.png',
+        image: '/assets/personal/code-projects/nball-sim.png',
         alt: 'gravity simulation',
         caption: 'Energy-conserving 2D Newtonian gravity simulator I built in high school.'
     },
     {
-        image: '/assets/personal/img/code-projects/pathviz2.png',
+        image: '/assets/personal/code-projects/pathviz2.png',
         alt: 'pathfinding visualization',
         caption: 'Pathfinding algorithm visualizer, featuring the most grating colorscheme of all time.'
     },
     {
-        image: '/assets/personal/img/blender/reactor.jpg',
+        image: '/assets/personal/blender/reactor.jpg',
         alt: 'nuclear reactor',
         caption: 'Still of a sci-fi reactor animation I digitally inserted into a real room, as a test of motion tracking.'
     },
     {
-        image: '/assets/personal/img/ricing/sathanas.png',
+        image: '/assets/personal/ricing/sathanas.png',
         alt: 'desktop',
         caption: 'My desktop with system stats dashboard displayed.'
     },
