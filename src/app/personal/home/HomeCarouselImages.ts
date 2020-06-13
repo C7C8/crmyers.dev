721a42e024c434e7246918ea349ea20de8f532ec
Sourec <tothemyers@gmail.com>
2020-06-12 21:12:05 -0400

    Remove duplicate images

--- src/app/personal/home/HomeCarouselImages.ts
+++ src/app/personal/home/HomeCarouselImages.ts
@@ -2,12 +2,12 @@ import { Image } from '../../common/defs';
 
 export const HomeCarouselImages: Image[] = [
     {
-        image: '/assets/personal/img/home-carousel/apisite.png',
+        image: '/assets/personal/img/code-projects/apisite.png',
         alt: 'API hosting site',
         caption: 'API hosting website built to help CS 3733 at WPI.'
     },
     {
-        image: '/assets/personal/img/home-carousel/cant-outrun-bots.jpg',
+        image: '/assets/personal/img/blender/cant-outrun-bots.jpg',
         alt: 'Robots destroying a city',
         caption: 'Art of some Amazon robots knocking down a city.'
     },
@@ -17,32 +17,32 @@ export const HomeCarouselImages: Image[] = [
         caption: 'Heatmap visualization of when I used my laptop in winter 2017.'
     },
     {
-        image: '/assets/personal/img/home-carousel/enterprise-ad-notexture.png',
+        image: '/assets/personal/img/blender/enterprise-ad-notexture.jpg',
         alt: 'USS Enterprise',
         caption: 'Untextured, unfinished mashup of the Enterprise and the Enterprise D.'
     },
     {
-        image: '/assets/personal/img/home-carousel/magnet_toy.jpg',
+        image: '/assets/personal/img/blender/magnet-toy.jpg',
         alt: 'magnet toy',
         caption: 'Render of a magnetic toy sitting on my desk.'
     },
     {
-        image: '/assets/personal/img/home-carousel/nball-sim.png',
+        image: '/assets/personal/img/code-projects/nball-sim.png',
         alt: 'gravity simulation',
         caption: 'Energy-conserving 2D Newtonian gravity simulator I built in high school.'
     },
     {
-        image: '/assets/personal/img/home-carousel/pathviz2.png',
+        image: '/assets/personal/img/code-projects/pathviz2.png',
         alt: 'pathfinding visualization',
         caption: 'Pathfinding algorithm visualizer, featuring the most grating colorscheme of all time.'
     },
     {
-        image: '/assets/personal/img/home-carousel/reactor.jpg',
+        image: '/assets/personal/img/blender/reactor.jpg',
         alt: 'nuclear reactor',
         caption: 'Still of a sci-fi reactor animation I digitally inserted into a real room, as a test of motion tracking.'
     },
     {
-        image: '/assets/personal/img/home-carousel/sathanas.png',
+        image: '/assets/personal/img/ricing/sathanas.png',
         alt: 'desktop',
         caption: 'My desktop with system stats dashboard displayed.'
     },
